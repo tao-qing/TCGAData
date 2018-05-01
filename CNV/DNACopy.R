a43555b352d7b35a57626d85e576cce6a39df681
QingTao <qingtao@QingTaodeMBP.wireless.yale.internal>
2018-05-01 09:17:40 -0400

    DNACopy
    DNACopy update

--- CNV/DNACopy.R
+++ CNV/DNACopy.R
@@ -1,46 +1,5 @@
-#setwd("/Users/qingtao/Analysis/R/YaleBRCA/TCGAData/CNV")
-#parameters
-args = commandArgs(trailingOnly=TRUE)
-
-cnvfile=args[1]
-spl_name=args[2]
-
-library(data.table)
-rawmat<-as.data.frame(fread(cnvfile,check.names=TRUE))
-matrmna<-rawmat[-which(is.na(rawmat[,6])),]
-matrmna[,6]<-round(log2((matrmna[,6])/2),digits=4)
-matrmna$test_tangent_1<-round(log2((matrmna$test_tangent_1)/2),digits=4)
-###################################Sigmentation analysis###########################################
-require(DNAcopy)
-#CNA.object <- CNA(as.numeric(as.matrix(matrmna[,6])),matrmna$CHROM,matrmna$POS,data.type="logratio",sampleid=spl_name)
-CNA.object <- CNA(matrmna[,6],matrmna$CHROM,matrmna$POS,data.type="logratio",sampleid="test_tangent_1")
-
-smoothed.CNA.object <- smooth.CNA(CNA.object)
-segment.smoothed.CNA.object <- segment(smoothed.CNA.object, verbose=1)
-
-png(paste0(spl_name,"_cnv.png"),width=2000,height=500)
-plot(segment.smoothed.CNA.object, plot.type="s",xmaploc=T)
-dev.off()
-
-###################################CNA annotation##################################################
-require(GenomicRanges)
-df = segment.smoothed.CNA.object$output
-cnv = makeGRangesFromDataFrame(df,keep.extra.columns=T)
-#cnv = GRanges(c("chr1", "chr2"), IRanges(94312388,244006886))
-
-require(Homo.sapiens)
-gns = geneRanges(Homo.sapiens, column="SYMBOL")
-
-output = CNVanno(gns, cnv, "SYMBOL")
-
-cnvMatanno<-output$cnvMatanno
-cnvGeneMat<-output$cnvGeneMat
-
-write.table(cnvMatanno,paste0(spl_name,"_cnvMatanno.txt"),sep="\t",row.names=F)
-write.table(cnvGeneMat,paste0(spl_name,"_cnvGeneMat.txt"),quote=F,sep="\t",row.names=F)
-
-geneRanges <- function(db, column="ENTREZID")
-  {
+###################################functions#####################################################
+geneRanges <- function(db, column="ENTREZID"){
     g <- genes(db, columns=column)
     col <- mcols(g)[[column]]
     genes <- granges(g)[rep(seq_along(g), elementNROWS(col))]
@@ -77,3 +36,42 @@ CNVanno <- function(query, subject, column="ENTREZID", ...){
     cnvGeneMat<- apply(cnvGeneMat,2,as.character)
     return(list(cnvMatanno=cnvMatanno,cnvGeneMat=cnvGeneMat))
 }
+
+#parameters
+args = commandArgs(trailingOnly=TRUE)
+
+cnvfile=args[1]
+spl_name=args[2]
+
+library(data.table)
+rawmat<-as.data.frame(fread(cnvfile))
+matrmna<-rawmat[-which(is.na(rawmat[,6])),]
+matrmna[,6]<-round(log2((matrmna[,6])/2),digits=4)
+###################################Sigmentation analysis###########################################
+require(DNAcopy)
+CNA.object <- CNA(as.numeric(as.matrix(matrmna[,6])),matrmna$CHROM,as.numeric(as.matrix(matrmna$POS)),data.type="logratio",sampleid=spl_name)
+smoothed.CNA.object <- smooth.CNA(CNA.object)
+segment.smoothed.CNA.object <- segment(smoothed.CNA.object, verbose=1)
+
+png(paste0(spl_name,"_cnv.png"),width=2000,height=500)
+plot(segment.smoothed.CNA.object, plot.type="s",xmaploc=T)
+dev.off()
+
+###################################CNA annotation##################################################
+require(GenomicRanges)
+df = segment.smoothed.CNA.object$output
+cnv = makeGRangesFromDataFrame(df,keep.extra.columns=T)
+#cnv = GRanges(c("chr1", "chr2"), IRanges(94312388,244006886))
+
+require(Homo.sapiens)
+gns = geneRanges(Homo.sapiens, column="SYMBOL")
+
+output = CNVanno(gns, cnv, "SYMBOL")
+
+cnvMatanno<-output$cnvMatanno
+cnvGeneMat<-output$cnvGeneMat
+
+write.table(cnvMatanno,paste0(spl_name,"_cnvMatanno.txt"),sep="\t",row.names=F)
+write.table(cnvGeneMat,paste0(spl_name,"_cnvGeneMat.txt"),quote=F,sep="\t",row.names=F)
+
+print (pasteo("Copy number annotation of ",spl_name," Finshed!")
