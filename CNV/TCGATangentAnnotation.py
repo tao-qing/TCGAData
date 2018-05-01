de91628e0a3b4a52bb4f7310ae0b1a80f657580e
QingTao <qingtao@QingTaodeMBP.wireless.yale.internal>
2018-05-01 14:22:50 -0400

    update 2018
    TCGATangentAnnotation update

--- CNV/TCGATangentAnnotation.py
+++ CNV/TCGATangentAnnotation.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python
 """
-Given a TCGA level 2 tangent normalized SNP array 6.0 data file, produce a annotation file for the target sample.
-
-@author tao <tao.qing@yale.edu>
-@date 4/17/2018
-@version 1.2
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
-the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
-and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+This software was modified based on the birdseed2VCF (https://github.com/ding-lab/birdseed2vcf), which was developed by Prof. Li Ding's lab
+Only used for annotation of TCGA tangent results.
 
 """
 
