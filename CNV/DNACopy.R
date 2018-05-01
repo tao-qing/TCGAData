9f0f393354a72734fb8b0a3c271b13ab233e15c0
QingTao <qingtao@QingTaodeMBP.wireless.yale.internal>
2018-05-01 08:47:45 -0400

    DNACopyAnnotation
    update DNACopy.R which was used for annotation of CNV in gene level

--- CNV/DNACopy.R
+++ CNV/DNACopy.R
@@ -1,26 +1,79 @@
+#setwd("/Users/qingtao/Analysis/R/YaleBRCA/TCGAData/CNV")
+#parameters
+args = commandArgs(trailingOnly=TRUE)
 
-setwd("/Users/qingtao/Analysis/R/YaleBRCA/TCGAData/CNV")
+cnvfile=args[1]
+spl_name=args[2]
+
 library(data.table)
-
-mat<-as.data.frame(fread("test_tangent"))
-matrmna<-mat[-which(is.na(mat$test_tangent_1)),]
+rawmat<-as.data.frame(fread(cnvfile,check.names=TRUE))
+matrmna<-rawmat[-which(is.na(rawmat[,6])),]
+matrmna[,6]<-round(log2((matrmna[,6])/2),digits=4)
 matrmna$test_tangent_1<-round(log2((matrmna$test_tangent_1)/2),digits=4)
-
-library(DNAcopy)
-
-
-CNA.object <- CNA(cbind(matrmna$test_tangent_1),matrmna$CHROM,matrmna$POS,data.type="logratio",sampleid="test_tangent_1")
+###################################Sigmentation analysis###########################################
+require(DNAcopy)
+#CNA.object <- CNA(as.numeric(as.matrix(matrmna[,6])),matrmna$CHROM,matrmna$POS,data.type="logratio",sampleid=spl_name)
+CNA.object <- CNA(matrmna[,6],matrmna$CHROM,matrmna$POS,data.type="logratio",sampleid="test_tangent_1")
 
 smoothed.CNA.object <- smooth.CNA(CNA.object)
 segment.smoothed.CNA.object <- segment(smoothed.CNA.object, verbose=1)
 
-
-png("cnv_test.png",width=2000,height=500)
-plot(segment.smoothed.CNA.object, plot.type="w")
+png(paste0(spl_name,"_cnv.png"),width=2000,height=500)
+plot(segment.smoothed.CNA.object, plot.type="s",xmaploc=T)
 dev.off()
 
+###################################CNA annotation##################################################
+require(GenomicRanges)
+df = segment.smoothed.CNA.object$output
+cnv = makeGRangesFromDataFrame(df,keep.extra.columns=T)
+#cnv = GRanges(c("chr1", "chr2"), IRanges(94312388,244006886))
 
+require(Homo.sapiens)
+gns = geneRanges(Homo.sapiens, column="SYMBOL")
 
-library(DNAcopy)
-data(coriell)
+output = CNVanno(gns, cnv, "SYMBOL")
 
+cnvMatanno<-output$cnvMatanno
+cnvGeneMat<-output$cnvGeneMat
+
+write.table(cnvMatanno,paste0(spl_name,"_cnvMatanno.txt"),sep="\t",row.names=F)
+write.table(cnvGeneMat,paste0(spl_name,"_cnvGeneMat.txt"),quote=F,sep="\t",row.names=F)
+
+geneRanges <- function(db, column="ENTREZID")
+  {
+    g <- genes(db, columns=column)
+    col <- mcols(g)[[column]]
+    genes <- granges(g)[rep(seq_along(g), elementNROWS(col))]
+    mcols(genes)[[column]] <- as.character(unlist(col))
+    genes
+  }
+
+CNVanno <- function(query, subject, column="ENTREZID", ...){
+    olaps <- findOverlaps(query, subject)
+    f1 <- factor(subjectHits(olaps),levels=seq_len(subjectLength(olaps)))
+    cnvMat<-as.data.frame(subject)
+    
+    hitedgenelist<-splitAsList(mcols(query)[[column]][queryHits(olaps)], f1)
+    hitedgenelistSep<-unlist(lapply(hitedgenelist,function(x)paste(x,collapse = "|")))
+    cnvMatanno<-cbind(cnvMat,hitedgenelistSep)
+
+    for(i in 1:dim(cnvMatanno)[1]){
+      iterm<-as.character(as.matrix(cnvMatanno[i,]))
+      genes=unlist(strsplit(iterm[9],split="\\|"))
+      if(i==1){
+        cnvGeneMat<-matrix(rep(iterm,length(genes)),ncol=9,byrow=T)
+        mergegene<-genes
+      }else{
+        cnvGeneMat<-rbind(cnvGeneMat,matrix(rep(cnvMatanno[i,],length(genes)),ncol=9,byrow=T))
+        mergegene<-c(mergegene,genes)
+      }
+    }
+    cnvGeneMat[,9]<-mergegene
+    cnvGeneMat<-as.data.frame(cnvGeneMat[,-5])
+    colnames(cnvGeneMat)<-c("chr","start","end","length","sample","NumbProbe","log2Ratio","Genes")
+    if(any(cnvGeneMat$Genes=="NA")){
+      cnvGeneMat<-cnvGeneMat[-which(cnvGeneMat$Genes=="NA"),]
+    }
+    cnvGeneMat<- apply(cnvGeneMat,2,as.character)
+    return(list(cnvMatanno=cnvMatanno,cnvGeneMat=cnvGeneMat))
+}
